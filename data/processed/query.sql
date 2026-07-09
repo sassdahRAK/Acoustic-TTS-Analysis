a1b1452b12705888034d99567dbf741e10d60fd7
sassdahrak123 <sassdahrak123@gmail.com>
2026-07-10 00:49:30 +0700

    analysis feature

--- data/processed/query.sql
+++ data/processed/query.sql
@@ -64,6 +64,7 @@ INNER JOIN load_raw_mos m
     ON w.filename = m.file;
 -- end: create new table 'wav_analysis' 
 
+-- XXX never run this query again
 -- see average mos
 SELECT AVG(MOS) FROM wav_analysis;
 -- end: 2.70202542874039
@@ -73,3 +74,38 @@ SELECT *
 FROM wav_analysis
 ORDER BY MOS DESC
 LIMIT 10;
+
+-- XXX never run this query agian
+-- add new atribute
+ALTER TABLE wav_attribute ADD COLUMN pitch_mean REAL;
+ALTER TABLE wav_attribute ADD COLUMN pitch_min REAL;
+ALTER TABLE wav_attribute ADD COLUMN pitch_max REAL;
+ALTER TABLE wav_attribute ADD COLUMN pitch_range REAL;
+
+ALTER TABLE wav_attribute ADD COLUMN energy_mean REAL;
+ALTER TABLE wav_attribute ADD COLUMN energy_range REAL;
+
+ALTER TABLE wav_attribute ADD COLUMN spectral_centroid REAL;
+ALTER TABLE wav_attribute ADD COLUMN spectral_bandwidth REAL;
+
+ALTER TABLE wav_attribute ADD COLUMN zcr REAL;
+
+
+--- sesect table row 
+SELECT COUNT(*)
+FROM wav_attribute;
+
+-- to show the info of table 
+PRAGMA table_info(wav_attribute);
+
+
+-- create new table 
+DROP TABLE IF EXISTS wav_analysis;
+
+CREATE TABLE wav_analysis AS
+SELECT
+    w.*,
+    m.MOS
+FROM wav_attribute w
+INNER JOIN load_raw_mos m
+ON w.file = m.file;
